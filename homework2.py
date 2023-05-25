4fb91e256f2c9e68342894b813de67e74570cd13
Jurgita Gestautas <jnj0205@gmail.com>
2023-05-25 15:42:05 -0500

    corrected mistakes

--- homework2.py
+++ homework2.py
@@ -3,17 +3,9 @@
 #Create a list of numbers that are less than ten using l_1 and list comprehension
 #Your output should [1,5,8,9]
 
-numbers = [1,11,14,5,8,9]
-
-for number in numbers:
-    num =[]
-    num.append(number)
-    if number <= 10:
-        print(num)
-
-
-
-
+numbers = [1, 11, 14, 5, 8, 9]
+less_than_ten = [num for num in numbers if num < 10]
+print(less_than_ten)
 
 
 #exercise 2
@@ -21,17 +13,10 @@ for number in numbers:
 # create a list of names of 4 letters or more and capitalize each name
 
 names = ['connor', 'connor', 'bob', 'connor', 'evan', 'max', 'evan', 'bob', 'kevin']
+new_names1 = [name.capitalize() for name in names if len(name) >=4]
+print(new_names1)
 
-for name in names:
-
-    print(name.capitalize())
+##########returning corrected
   
 
 
-
-## Bonus Challenge
-names2 = ['connor', 'connor', 'bob', 'connor', 'evan', 'max', 'evan', 2, 2, 2, 3, 3, 4, 'bob', 'kevin']
-string = 'alphanumeric'
-for name1 in names2:
-
-    print(name1.capitalize(), string())
